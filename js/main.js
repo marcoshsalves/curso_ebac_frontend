fd6e37769d63faef4d4eb2834af57ff49bd34898
Marcos Alves <92824549+marcoshsalves@users.noreply.github.com>
2023-02-17 20:31:51 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('#cpf').mask('000.000.000-00');
+
+    $('#cep').mask('00000-000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira um e-mail válido',
+            endereco: 'Por favor, insira seu endereço'
+        }
+    })
+})
